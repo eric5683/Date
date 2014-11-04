d5fc9408d207711d1ee18d20e072a4371a2dab0c
Eric Sanchez <ericsanchez@outlook.com>
2014-11-04 00:25:04 -0500

    Initial Commit

---
+++ Date/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Date
+//
+//  Created by Eric Sanchez on 11/4/14.
+//  Copyright (c) 2014 Eric. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
